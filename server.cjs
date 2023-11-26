37ddc23b38c1dba6d8a71b204b14e4bcf9d76631
themess69two@gmail.com <themess69two@gmail.com>
2023-11-26 16:48:11 +0100

    Server

---
+++ server.cjs
@@ -0,0 +1,15 @@
+var http=require('http');
+var url=require('url');
+
+var server=http.createServer(function(req,res){
+    var pathname=url.parse(req.url).pathname;
+    switch(pathname){
+        case '/subpage':
+            res.end('subpage');
+        break;
+        default:
+            res.end('default');
+        break;
+    }
+
+}).listen(8080);
